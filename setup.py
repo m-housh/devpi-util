87f7acf2c40a1e86191c362fc855c69c62391ea0
Michael Housh <mhoush@houshhomeenergy.com>
2016-05-27 10:33:36 -0400

    Updated setup.py to inclued author and author-email

--- setup.py
+++ setup.py
@@ -3,6 +3,8 @@ from setuptools import setup, find_packages
 setup(
         name='devpi-client-util',
         version='0.1.0',
+        author='Michael Housh',
+        author_email='mhoush@houshhomeenergy.com',
         packages=find_packages(),
         zip_safe=False,
         install_requires=[
